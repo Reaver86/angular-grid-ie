d2ef2e4b2c2074f6706a6316a19987ba4c658365
Alexander Schuster <alexander.schuster@allianz.de>
2018-10-31 17:39:28 +0100

    prepare for heroku and add media queries to grid

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const distPath = path.join(__dirname, '..', 'dist', 'angular-grid-ie');
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+
+app.use('/', express.static(distPath));
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
